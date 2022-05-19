e252f62334f89289adf6a08703237ba98bb8de1b
Podbrushkin <podbrushkin@mail.gov>
2022-05-19 18:45:00 +0300

    Now parser works and refactored, parseSmallToken() added

--- Parser.java
+++ Parser.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.Objects;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.charset.Charset;
@@ -17,6 +18,8 @@ import java.io.IOException;
 
 
 public class Parser {
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.forLanguageTag("ru"));
+	DateTimeFormatter dtfAsIs = DateTimeFormatter.ofPattern("d LLLL yyyy", Locale.forLanguageTag("ru"));
 	public Set<LocalDate> parseFromCurrentFolder() {
 		
 		
@@ -44,62 +47,69 @@ public class Parser {
 	public Set<LocalDate> parseFile(Path file) throws IOException {
 		Set<LocalDate> dates = new TreeSet<LocalDate>();
 		Charset charset = Charset.forName(UniversalDetector.detectCharset(file));
-		// System.out.println("Detected charset: " +charset);
 		List<String> lines = Files.lines(file, charset).map(l -> l.strip()).filter(l -> l.length() > 3).toList();
-		var dtf = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.forLanguageTag("ru"));
+		
 		for (String line : lines) {
-			// String[] tokens = line.split("[ \t\r\n]");
-			LocalDate ldt;
-			//if line has 1+ commas
-			// (zero&more of not-commas, comma, zero&more of not-commas) 2 and more times in string
-			// if (line.matches("^([^,]*,[^,]*){2,}$")) {
+			//---FIRST TRY full line
+			dates.addAll(parseSmallToken(line));
+			
+			//---SECOND TRY comma-separated
 			if (line.split(",", -1).length-1 >= 1) {
-				// System.out.printf("\nLine matched: >>%s<<", line);
 				var st = new StringTokenizer(line, ",");
 				while (st.hasMoreTokens()) {
-					try {
-						var tk = st.nextToken().strip();
-						// System.out.println("token is: "+tk);
-						ldt = LocalDate.parse(tk, dtf);
-						ldt = DateParserUtils.parseDateTime(tk).toLocalDate();
-						// if (ldt != null) dates.add(ldt.toLocalDate());
-						if (ldt != null) {
-							System.out.printf("LDT parsed: %s, from line:%s\n",ldt,line);
-							dates.add(ldt);
-							ldt = null;
-						}
-					} catch (java.time.format.DateTimeParseException e) {}
+					var tk = st.nextToken();
+					dates.addAll(parseSmallToken(tk));
 				}
 			}
 			
-			try {
-				ldt = DateParserUtils.parseDateTime(line.strip()).toLocalDate();
-				if (ldt != null) {
-					System.out.printf("LDT parsed: %s, from line:%s\n",ldt,line);
-					dates.add(ldt);
-					ldt = null;
-				}
-			} catch (java.time.format.DateTimeParseException e) {}
-			
+			//---THIRD TRY space-separated
 			var st = new StringTokenizer(line);
 			while (st.hasMoreTokens()) {
-				String curToken = st.nextToken();
-				/* String compound = curToken;
-				if (curToken.chars().anyMatch( Character::isDigit ) && st.hasMoreTokens()) {
-					compound = curToken + st.nextToken();
-				} */
-				try {
-					ldt = DateParserUtils.parseDateTime(curToken).toLocalDate();
-					if (ldt != null) {
-						System.out.printf("LDT parsed: %s, from line:%s\n",ldt,line);
-						dates.add(ldt);
-						ldt = null;
-					}
-				} catch (java.time.format.DateTimeParseException e) {}
-				
+				String tk = st.nextToken();
+				if (tk.length() < 6) continue;
+				dates.addAll(parseSmallToken(tk));
+			}
+			
+			//---FOURTH TRY comma- or tab-separated
+			st = new StringTokenizer(line, ",\t");
+			while (st.hasMoreTokens()) {
+				String tk = st.nextToken();
+				if (tk.length() < 6) continue;
+				dates.addAll(parseSmallToken(tk));
 			}
 		}
+		
 		dates.removeIf(d -> d.getYear() == 0);
 		return dates;
+	}
+	
+	
+	
+	private Set<LocalDate> parseSmallToken(String token) {
+		token = token.strip();
+		var tempDates = new TreeSet<LocalDate>();
+		//1st and 2nd tries allow parsing from russian
+		try {
+			var ld = LocalDate.parse(token.toLowerCase(), dtf);
+			tempDates.add(ld);
+		} catch (java.time.format.DateTimeParseException e) {}
+		
+		if (tempDates.size() == 0) {
+			try {
+				var ld = LocalDate.parse(token.toLowerCase(), dtfAsIs);
+				tempDates.add(ld);
+			} catch (java.time.format.DateTimeParseException e) {}
+		}
+		
+		try {
+			var ld = DateParserUtils.parseDateTime(token).toLocalDate();
+			if (ld != null) {
+				// System.out.printf("%s\t parsed from: %s\n",ld,token);
+				tempDates.add(ld);
+			}
+		} catch (java.time.format.DateTimeParseException e) {}
+		if (tempDates.size() > 0)
+		System.out.printf("%s\t parsed from: %s\n",tempDates,token);
+		return tempDates;
 	}
 }
