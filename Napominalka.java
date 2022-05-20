ce992e99731342157f5756c5608f5500754473ae
Podbrushkin <podbrushkin@mail.gov>
2022-05-20 17:39:23 +0300

    Now info from tsv is displayed in main window

--- Napominalka.java
+++ Napominalka.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.TreeMap;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Napominalka {
 	private TreeMap<LocalDate, String> datesNames = DatesNamesContainer.getDatesNames();
@@ -17,6 +18,8 @@ public class Napominalka {
 	}
 	
 	private void buildGui() {
+		// UIManager.getLookAndFeelDefaults().put("defaultFont", new Font("Arial", Font.BOLD, 24));
+		UIManager.put("TextField.font", new Font("Arial", Font.BOLD, 17));
 		var frame = new JFrame("Напоминалка");
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,18 +28,19 @@ public class Napominalka {
 		// mainWindow.setLayout(new GridLayout(datesNames.size(),2));
 		BorderLayout layout = new BorderLayout();
 		JPanel background = new JPanel(layout);
-		background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		background.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
 		
 		GridLayout grid = new GridLayout(datesNames.size(), 2);
 		grid.setVgap(1);
-		grid.setHgap(2);
+		grid.setHgap(10);
 		var mainPanel = new JPanel(grid);
 		background.add(BorderLayout.CENTER, mainPanel);
 		frame.getContentPane().add(background);
 		
-		for (int i = 0; i < datesNames.size(); i++) {
-			var entry = datesNames.pollFirstEntry();
-			var textField = new JTextField(entry.getKey().toString());
+		var tmpMap = new TreeMap<>(datesNames);
+		while (!tmpMap.isEmpty()) {
+			var entry = tmpMap.pollFirstEntry();
+			var textField = new JTextField(entry.getKey().format(DateTimeFormatter.ofPattern("dd MMMM yyyy")));
 			textField.setEditable(false);
 			mainPanel.add(textField);
 			textField = new JTextField(entry.getValue().toString());
@@ -46,6 +50,7 @@ public class Napominalka {
 		}
 		
 		frame.pack();
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
 }
