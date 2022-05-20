af51d79b5624d21bb7726c8cbbc8ff46c330a882
Podbrushkin <podbrushkin@mail.gov>
2022-05-20 21:38:20 +0300

    Added TrayIcon, fonts auto-scaling and .bat for comp&run

--- Napominalka.java
+++ Napominalka.java
@@ -1,25 +1,42 @@
 package napominalka;
 
 import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.TreeMap;
+import java.util.Arrays;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Napominalka {
 	private TreeMap<LocalDate, String> datesNames = DatesNamesContainer.getDatesNames();
+	private final Font defFont = Font.decode(null);
+	private float scaleRatio = Toolkit.getDefaultToolkit().getScreenResolution()/96;
+	private float scaleAdditional = 2.5f;
+	private float newFontSize = defFont.getSize() * scaleRatio * scaleAdditional;
 	
+	// private final Font scaledFont = defFont.deriveFont(newFontSize);
+	private final Font scaledFont = new Font("Calibri", Font.PLAIN, (int)newFontSize);
 	public static void main(String[] args) {
 		
 		
 		new Napominalka().buildGui();
-		
+		// printAvailableFonts();
+		// System.out.println("getScreenResolution:"+Toolkit.getDefaultToolkit().getScreenResolution());
+		// System.out.println("scaledFont:"+scaledFont);
 		
 	}
 	
 	private void buildGui() {
-		// UIManager.getLookAndFeelDefaults().put("defaultFont", new Font("Arial", Font.BOLD, 24));
-		UIManager.put("TextField.font", new Font("Arial", Font.BOLD, 17));
+		// UIManager.put("defaultFont", defFont.deriveFont(30f));
+		// UIManager.getLookAndFeelDefaults().put("defaultFont", defFont.deriveFont(30f));
+		// UIManager.getDefaults().put("defaultFont", defFont.deriveFont(30f));
+		/* UIManager.put("TextField.font", new Font("Arial", Font.BOLD, 17));
+		UIManager.put("MenuItem.font", new Font("Arial", Font.BOLD, 17));
+		UIManager.put("Menu.font", new Font("Arial", Font.BOLD, 17));
+		UIManager.put("PopupMenu.font", new Font("Arial", Font.BOLD, 17)); */
+		setUIFont(new FontUIResource(scaledFont));
 		var frame = new JFrame("Напоминалка");
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,9 +65,83 @@ public class Napominalka {
 			mainPanel.add(textField);
 			
 		}
-		
+		addTrayIcon();
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
+	
+	private void addTrayIcon() {
+		
+		TrayIcon trayIcon = null;
+		if (SystemTray.isSupported()) {
+			
+			
+			SystemTray tray = SystemTray.getSystemTray();
+			Image image = Toolkit.getDefaultToolkit().getImage("icon.png");
+			
+			// create a action listener to listen for default action executed on the tray icon
+			/* ActionListener listener = new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					System.out.println("listenre action fired");
+					// execute default action of the application
+					// ...
+				}
+			}; */
+			
+			PopupMenu popup = new PopupMenu("Название попуп меню");
+			popup.setFont(scaledFont.deriveFont(scaledFont.getSize()*0.6f));
+			
+			MenuItem mainItem = new MenuItem("Also Exit");
+			
+			mainItem.addActionListener((ae) -> System.exit(0));
+			popup.add(mainItem);
+			
+			popup.addSeparator();
+			MenuItem exitItem = new MenuItem("Exit");
+			
+			exitItem.addActionListener((ae) -> System.exit(0));
+			popup.add(exitItem);
+			
+			
+			trayIcon = new TrayIcon(image, "Напоминалка", popup);
+			trayIcon.setImageAutoSize(true);
+			trayIcon.addMouseListener(new MouseListener() {
+				public void mouseClicked(MouseEvent e) {
+					System.out.println("Cliked!"+e.getButton());
+				}
+				public void mouseEntered(MouseEvent e) {}
+				public void mouseExited(MouseEvent e) {}
+				public void mousePressed(MouseEvent e) {}
+				public void mouseReleased(MouseEvent e) {}
+			});
+			try {
+				tray.add(trayIcon);
+			} catch (AWTException e) {
+				System.err.println(e);
+			}
+		}
+		
+	}
+	
+	public static void setUIFont (FontUIResource f){
+		java.util.Enumeration keys = UIManager.getDefaults().keys();
+		while (keys.hasMoreElements()) {
+		  Object key = keys.nextElement();
+		  Object value = UIManager.get(key);
+		  if (value instanceof javax.swing.plaf.FontUIResource)
+			UIManager.put(key, f);
+		  }
+    }
+	
+	private static void printAvailableFonts() {
+		var lge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		// System.out.println(".getAllFonts():"+Arrays.toString(lge.getAllFonts()));
+		// System.out.println(".getAvailableFontFamilyNames():"+Arrays.toString(lge.getAvailableFontFamilyNames()));
+		
+		for (String name : lge.getAvailableFontFamilyNames()) {
+			System.out.println(name);
+		}
+	}
+
 }
---
+++ comp&run.bat
@@ -0,0 +1,2 @@
+rmdir /S /Q classes & javac -cp lib\* *.java -d classes && java -Dsun.java2d.uiScale=1 -cp .;lib\*;classes napominalka.Napominalka
+cmd.exe /k
